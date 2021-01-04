be6d030e2adeee695769b2df09567610ef81a1be
xwjBupt <xwj_bupt@163.com>
2021-01-04 11:11:16 +0800

    add batch for eval

---
+++ auto.sh
@@ -0,0 +1,144 @@
+#!/bin/bash
+
+
+# echo -e ">>>> \n\n" 
+# echo 'epoch_90_weights.pth'
+# python configs/trainval/tinaface/test_widerface.py configs/trainval/tinaface/tinaface.py /dfsdata2/xuwj16_data/vedadet/workdir/tinaface/epoch_90_weights.pth
+# echo -e "<<<< \n\n" 
+
+# echo -e ">>>> \n\n" 
+# echo 'epoch_100_weights.pth'
+# python configs/trainval/tinaface/test_widerface.py configs/trainval/tinaface/tinaface.py /dfsdata2/xuwj16_data/vedadet/workdir/tinaface/epoch_100_weights.pth
+# echo -e "<<<< \n\n" 
+# echo -e ">>>> \n\n" 
+
+# echo 'epoch_110_weights.pth'
+# python configs/trainval/tinaface/test_widerface.py configs/trainval/tinaface/tinaface.py /dfsdata2/xuwj16_data/vedadet/workdir/tinaface/epoch_110_weights.pth
+# echo -e "<<<< \n\n" 
+# echo -e ">>>> \n\n" 
+
+# echo 'epoch_120_weights.pth'
+# python configs/trainval/tinaface/test_widerface.py configs/trainval/tinaface/tinaface.py /dfsdata2/xuwj16_data/vedadet/workdir/tinaface/epoch_120_weights.pth
+# echo -e "<<<< \n\n" 
+# echo -e ">>>> \n\n" 
+
+# echo 'epoch_120_weights.pth'
+# python configs/trainval/tinaface/test_widerface.py configs/trainval/tinaface/tinaface.py /dfsdata2/xuwj16_data/vedadet/workdir/tinaface/epoch_120_weights.pth
+# echo -e "<<<< \n\n" 
+# echo -e ">>>> \n\n" 
+
+# echo 'epoch_120_weights.pth'
+# python configs/trainval/tinaface/test_widerface.py configs/trainval/tinaface/tinaface.py /dfsdata2/xuwj16_data/vedadet/workdir/tinaface/epoch_120_weights.pth
+# echo -e "<<<< \n\n" 
+# echo -e ">>>> \n\n" 
+
+# echo 'epoch_125_weights.pth'
+# python configs/trainval/tinaface/test_widerface.py configs/trainval/tinaface/tinaface.py /dfsdata2/xuwj16_data/vedadet/workdir/tinaface/epoch_125_weights.pth
+# echo -e "<<<< \n\n" 
+# echo -e ">>>> \n\n" 
+
+# echo 'epoch_130_weights.pth'
+# python configs/trainval/tinaface/test_widerface.py configs/trainval/tinaface/tinaface.py /dfsdata2/xuwj16_data/vedadet/workdir/tinaface/epoch_130_weights.pth
+# echo -e "<<<< \n\n" 
+# echo -e ">>>> \n\n" 
+
+# echo 'epoch_135_weights.pth'
+# python configs/trainval/tinaface/test_widerface.py configs/trainval/tinaface/tinaface.py /dfsdata2/xuwj16_data/vedadet/workdir/tinaface/epoch_135_weights.pth
+# echo -e "<<<< \n\n" 
+# echo -e ">>>> \n\n" 
+
+# echo 'epoch_140_weights.pth'
+# python configs/trainval/tinaface/test_widerface.py configs/trainval/tinaface/tinaface.py /dfsdata2/xuwj16_data/vedadet/workdir/tinaface/epoch_140_weights.pth
+# echo -e "<<<< \n\n" 
+# echo -e ">>>> \n\n" 
+
+# echo 'epoch_145_weights.pth'
+# python configs/trainval/tinaface/test_widerface.py configs/trainval/tinaface/tinaface.py /dfsdata2/xuwj16_data/vedadet/workdir/tinaface/epoch_145_weights.pth
+# echo -e "<<<< \n\n" 
+
+
+
+# echo -e ">>>> \n\n" 
+# echo 'epoch_150_weights.pth'
+# python configs/trainval/tinaface/test_widerface.py configs/trainval/tinaface/tinaface.py /dfsdata2/xuwj16_data/vedadet/workdir/tinaface/epoch_150_weights.pth
+# echo -e "<<<< \n\n" 
+
+# echo -e ">>>> \n\n" 
+# echo 'epoch_155_weights.pth'
+# python configs/trainval/tinaface/test_widerface.py configs/trainval/tinaface/tinaface.py /dfsdata2/xuwj16_data/vedadet/workdir/tinaface/epoch_155_weights.pth
+# echo -e "<<<< \n\n" 
+
+# echo -e ">>>> \n\n" 
+# echo 'epoch_160_weights.pth'
+# python configs/trainval/tinaface/test_widerface.py configs/trainval/tinaface/tinaface.py /dfsdata2/xuwj16_data/vedadet/workdir/tinaface/epoch_160_weights.pth
+# echo -e "<<<< \n\n" 
+
+# echo -e ">>>> \n\n" 
+# echo 'epoch_165_weights.pth'
+# python configs/trainval/tinaface/test_widerface.py configs/trainval/tinaface/tinaface.py /dfsdata2/xuwj16_data/vedadet/workdir/tinaface/epoch_165_weights.pth
+# echo -e "<<<< \n\n" 
+
+# echo -e ">>>> \n\n" 
+# echo 'epoch_170_weights.pth'
+# python configs/trainval/tinaface/test_widerface.py configs/trainval/tinaface/tinaface.py /dfsdata2/xuwj16_data/vedadet/workdir/tinaface/epoch_170_weights.pth
+# echo -e "<<<< \n\n" 
+
+# echo -e ">>>> \n\n" 
+# echo 'epoch_175_weights.pth'
+# python configs/trainval/tinaface/test_widerface.py configs/trainval/tinaface/tinaface.py /dfsdata2/xuwj16_data/vedadet/workdir/tinaface/epoch_175_weights.pth
+# echo -e "<<<< \n\n" 
+
+# echo -e ">>>> \n\n" 
+# echo 'epoch_180_weights.pth'
+# python configs/trainval/tinaface/test_widerface.py configs/trainval/tinaface/tinaface.py /dfsdata2/xuwj16_data/vedadet/workdir/tinaface/epoch_180_weights.pth
+# echo -e "<<<< \n\n" 
+
+# echo -e ">>>> \n\n" 
+# echo 'epoch_185_weights.pth'
+# python configs/trainval/tinaface/test_widerface.py configs/trainval/tinaface/tinaface.py /dfsdata2/xuwj16_data/vedadet/workdir/tinaface/epoch_185_weights.pth
+# echo -e "<<<< \n\n" 
+
+# echo -e ">>>> \n\n" 
+# echo 'epoch_190_weights.pth'
+# python configs/trainval/tinaface/test_widerface.py configs/trainval/tinaface/tinaface.py /dfsdata2/xuwj16_data/vedadet/workdir/tinaface/epoch_190_weights.pth
+# echo -e "<<<< \n\n" 
+
+echo -e ">>>> \n\n" 
+echo 'epoch_176_weights.pth'
+python configs/trainval/tinaface/test_widerface.py configs/trainval/tinaface/tinaface.py /dfsdata2/xuwj16_data/vedadet/workdir/tinaface/epoch_176_weights.pth
+echo -e "<<<< \n\n" 
+
+echo -e ">>>> \n\n" 
+echo 'epoch_177_weights.pth'
+python configs/trainval/tinaface/test_widerface.py configs/trainval/tinaface/tinaface.py /dfsdata2/xuwj16_data/vedadet/workdir/tinaface/epoch_177_weights.pth
+echo -e "<<<< \n\n" 
+
+echo -e ">>>> \n\n" 
+echo 'epoch_178_weights.pth'
+python configs/trainval/tinaface/test_widerface.py configs/trainval/tinaface/tinaface.py /dfsdata2/xuwj16_data/vedadet/workdir/tinaface/epoch_178_weights.pth
+echo -e "<<<< \n\n" 
+
+echo -e ">>>> \n\n" 
+echo 'epoch_179_weights.pth'
+python configs/trainval/tinaface/test_widerface.py configs/trainval/tinaface/tinaface.py /dfsdata2/xuwj16_data/vedadet/workdir/tinaface/epoch_179_weights.pth
+echo -e "<<<< \n\n" 
+
+echo -e ">>>> \n\n" 
+echo 'epoch_181_weights.pth'
+python configs/trainval/tinaface/test_widerface.py configs/trainval/tinaface/tinaface.py /dfsdata2/xuwj16_data/vedadet/workdir/tinaface/epoch_181_weights.pth
+echo -e "<<<< \n\n" 
+
+echo -e ">>>> \n\n" 
+echo 'epoch_182_weights.pth'
+python configs/trainval/tinaface/test_widerface.py configs/trainval/tinaface/tinaface.py /dfsdata2/xuwj16_data/vedadet/workdir/tinaface/epoch_182_weights.pth
+echo -e "<<<< \n\n" 
+
+echo -e ">>>> \n\n" 
+echo 'epoch_183_weights.pth'
+python configs/trainval/tinaface/test_widerface.py configs/trainval/tinaface/tinaface.py /dfsdata2/xuwj16_data/vedadet/workdir/tinaface/epoch_183_weights.pth
+echo -e "<<<< \n\n" 
+
+echo -e ">>>> \n\n" 
+echo 'epoch_184_weights.pth'
+python configs/trainval/tinaface/test_widerface.py configs/trainval/tinaface/tinaface.py /dfsdata2/xuwj16_data/vedadet/workdir/tinaface/epoch_184_weights.pth
+echo -e "<<<< \n\n" 
